f8d930c33f0197bccd7a7fb60aa3dd3373174d5f
dixrtech-lab <dixrtech@gmail.com>
2026-01-06 08:16:10 +0100

    Create 02_create_schemas.sql

---
+++ scripts/02_create_schemas.sql
@@ -0,0 +1,6 @@
+/* Create Schemas (bronze, silver & gold)
+Run this inside data_warehouse */
+
+CREATE SCHEMA IF NOT EXISTS bronze;
+CREATE SCHEMA IF NOT EXISTS silver;
+CREATE SCHEMA IF NOT EXISTS gold;
